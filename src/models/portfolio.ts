c42b6ed8289ded05557ab35c9385f41d5e10e323
yangseungbin <tanja1206@naver.com>
2020-11-26 15:19:54 +0900

    [ADD] portfolio entity

---
+++ src/models/portfolio.ts
@@ -0,0 +1,41 @@
+import Sequelize, { Model } from "sequelize";
+import { sequelize } from "../config/config";
+import { User } from "./user";
+
+export class Portfolio extends Model {
+  id: number;
+  giturl: string;
+  introduce: string;
+  title: string;
+  userEmail: string;
+}
+
+Portfolio.init(
+  {
+    id: {
+      type: Sequelize.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    giturl: {
+      type: Sequelize.STRING(50),
+    },
+    introduce: {
+      type: Sequelize.STRING(1000),
+    },
+    title: {
+      type: Sequelize.STRING(50),
+    },
+    userEmail: {
+      type: Sequelize.STRING(50),
+      field: "user_email",
+    },
+  },
+  { sequelize, modelName: "portfolio" }
+);
+
+Portfolio.hasOne(User, { foreignKey: "firstPortfolioId", sourceKey: "id" });
+User.belongsTo(Portfolio, { foreignKey: "firstPortfolioId" });
+
+Portfolio.hasOne(User, { foreignKey: "secondPortfolioId", sourceKey: "id" });
+User.belongsTo(Portfolio, { foreignKey: "secondPortfolioId" });
