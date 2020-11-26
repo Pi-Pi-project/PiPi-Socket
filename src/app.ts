0f609d2d7221290557d8cbb88a8273e8b1b1910e
yangseungbin <tanja1206@naver.com>
2020-11-26 15:38:54 +0900

    [ADD] connect db & router

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import express, { Application, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import http from "http";
 import socketio from "socket.io";
+import * as dotenv from "dotenv";
+import path from "path";
+import { sequelize } from "./config/config";
+import router from "./routes";
+
+dotenv.config({ path: path.join(__dirname + "../../.env") });
 
 const app: Application = express();
 
@@ -11,6 +17,14 @@ const io = socketio(server);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+sequelize.sync();
+
+app.use("/", router);
+
+app.use((err, req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: err.message });
+});
 
 io.sockets.on("connection", (socket) => {
   console.log(1);
