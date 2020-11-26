c3d709b0f9c32118305b41ecbdb32f36b5c92396
yangseungbin <tanja1206@naver.com>
2020-11-26 15:20:22 +0900

    [ADD] chat entity

---
+++ src/models/chat.ts
@@ -0,0 +1,31 @@
+import Sequelize, { Model } from "sequelize";
+import { sequelize } from "../config/config";
+
+export class Chat extends Model {
+  id: number;
+  message: string;
+  roomId: number;
+  userEmail: string;
+}
+
+Chat.init(
+  {
+    id: {
+      type: Sequelize.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    message: {
+      type: Sequelize.STRING(500),
+    },
+    roomId: {
+      type: Sequelize.BIGINT,
+      field: "room_id",
+    },
+    userEmail: {
+      type: Sequelize.STRING(50),
+      field: "user_email",
+    },
+  },
+  { sequelize, modelName: "chat" }
+);
