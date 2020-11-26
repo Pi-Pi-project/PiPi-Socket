f83a0927aa530307eb14a69929eab4f3bad6b832
yangseungbin <tanja1206@naver.com>
2020-11-26 15:19:22 +0900

    [ADD] room entity

---
+++ src/models/room.ts
@@ -0,0 +1,34 @@
+import Sequelize, { Model } from "sequelize";
+import { sequelize } from "../config/config";
+import { Chat } from "./chat";
+import { ChatMember } from "./chatMember";
+
+export class Room extends Model {
+  id: number;
+  title: string;
+  coverImg: string;
+}
+
+Room.init(
+  {
+    id: {
+      type: Sequelize.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: Sequelize.STRING(50),
+    },
+    coverImg: {
+      type: Sequelize.STRING(50),
+      field: "cover_img",
+    },
+  },
+  { sequelize, modelName: "room" }
+);
+
+Room.hasMany(ChatMember, { foreignKey: "roomId", sourceKey: "id" });
+ChatMember.belongsTo(Room, { foreignKey: "roomId" });
+
+Room.hasMany(Chat, { foreignKey: "roomId", sourceKey: "id" });
+Chat.belongsTo(Room, { foreignKey: "roomId" });
