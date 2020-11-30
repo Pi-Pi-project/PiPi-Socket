2fffb9c108c2473ca9747713b5eeb6814860fc4c
yangseungbin <tanja1206@naver.com>
2020-11-30 17:02:06 +0900

    [MODIFY] dummy method

--- src/services/chat.ts
+++ src/services/chat.ts
@@ -1,11 +1,9 @@
 import { Chat } from "../models/chat";
-import { User } from "../models/user";
 import { Room } from "../models/room";
 import { ChatRequestDTO } from "../interfaces/chat";
 
 export const chat = async (req: ChatRequestDTO) => {
   await Chat.create({
-    id: 1,
     roomId: req.roomId,
     userEmail: req.userEmail,
     message: req.message,
@@ -13,6 +11,5 @@ export const chat = async (req: ChatRequestDTO) => {
 };
 
 export const dummy = async () => {
-  await User.findOne();
   await Room.findOne();
 };
